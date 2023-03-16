a2e5670680fbdca893434aa3c805fdcbf5e9c1e3
Lennna <pampam32121@gmail.com>
2023-03-16 22:34:23 +0200

    refactored MyForm using Formik components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './app.scss';
-import Form from './Form';
+import MyForm from './Form';
 
 function App() {
   return (
     <div className="app">
-        <Form/>
+        <MyForm/>
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,17 +1,10 @@
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-const Form = () => {
-    const formik = useFormik({
-        initialValues: {
-          name: '',
-          email: '',
-          amount: 0,
-          currency: '',
-          text: '',
-          terms: false
-        },
-        validationSchema: Yup.object({
+const MyForm = () => (
+    <Formik
+        initialValues={{ name: '', email: '', amount: 0, currency: '', text: '', terms: false }}
+        validationSchema={Yup.object({
             name: Yup.string()
                 .min(2, 'The name is too short')
                 .required('Required'),
@@ -28,79 +21,50 @@ const Form = () => {
             terms: Yup.boolean()
                 .oneOf([true], "The terms and conditions must be accepted.")
                 .required('You have to accept our privacy terms')
-        }),
-        onSubmit: values => console.log(JSON.stringify(values, null, 2)),
-    });
+        })} 
+        onSubmit={values => console.log(JSON.stringify(values, null, 2))}
+    >
+        {({ isSubmitting }) => (
+            <Form className="form">
+                <h2>Donate</h2>
 
-    return (
-        <form className="form" onSubmit={formik.handleSubmit}>
-            <h2>Donate</h2>
-            <label htmlFor="name">Your name</label>
-            <input
-                id="name"
-                name="name"
-                type="text"
-                value={formik.values.name}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-            />
-            {formik.errors.name && formik.touched.name && <div className="error">{formik.errors.name}</div>}
-            <label htmlFor="email">Email</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-            />
-            {formik.errors.email && formik.touched.email && <div className="error">{formik.errors.email}</div>}
-            <label htmlFor="amount">Sum</label>
-            <input
-                id="amount"
-                name="amount"
-                type="number"
-                value={formik.values.amount}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-            />
-            {formik.errors.amount && formik.touched.amount && <div className="error">{formik.errors.amount}</div>}
-            <label htmlFor="currency">Currency</label>
-            <select
-                id="currency"
-                name="currency"
-                value={formik.values.currency}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-            >
+                <label htmlFor="name">Your name</label>
+                <Field type="text" name="name" id="name" />
+                <ErrorMessage name="name" component="div" className="error" />
+
+                <label htmlFor="email">Email</label>
+                <Field type="email" name="email" id="email" />
+                <ErrorMessage name="email" component="div" className="error" />
+
+                <label htmlFor="amount">Sum</label>
+                <Field type="number" name="amount" id="amount" />
+                <ErrorMessage name="amount" component="div" className="error" />  
+
+                <label htmlFor="currency">Currency</label>
+                <Field as="select" name="currency" id="currency">
                     <option value="">Choose currency</option>
                     <option value="USD">USD</option>
                     <option value="UAH">UAH</option>
                     <option value="EUR">EUR</option>
-            </select>
-            {formik.errors.currency && formik.touched.currency && <div className="error">{formik.errors.currency}</div>}
-            <label htmlFor="text">Leave a message</label>
-            <textarea 
-                id="text"
-                name="text"
-                value={formik.values.text}
-                onChange={formik.handleChange}
-            />
-            {formik.errors.text && formik.touched.text && <div className="error">{formik.errors.text}</div>}
-            <label className="checkbox">
-                <input 
-                    name="terms" 
-                    type="checkbox" 
-                    value={formik.values.terms}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                />
-                Accept our privacy policy terms
-            </label>
-            {formik.errors.terms && formik.touched.terms && <div className="error">{formik.errors.terms}</div>}
-            <button type="submit">Send</button>
-        </form>
-    )
-}
+                </Field>
+                <ErrorMessage name="currency" component="div" className="error" />
 
-export default Form;
+                <label htmlFor="text">Leave a message</label>
+                <Field as="textarea" name="text" id="text" />
+                <ErrorMessage name="text" component="div" className="error" />
+
+                <label className="checkbox">
+                    <Field type="checkbox" name="terms" id="terms" />
+                    Accept our privacy policy terms
+                </label>
+                <ErrorMessage name="terms" component="div" className="error" />
+
+                <button type="submit" disabled={isSubmitting}>
+                    Send
+                </button>
+            </Form>
+        )}
+    </Formik>
+)
+
+export default MyForm;
